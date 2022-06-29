492f6192f7247c394158198e4dd3e2ab35a3b3a5
Mohammadreza Amini <mohammadreza99a@yahoo.com>
2022-06-29 13:30:26 +0200

    formatted some logging outputs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -11,19 +11,21 @@ if (!existsSync(logDir)) {
   mkdirSync(logDir);
 }
 
-// Define log format
-const logFormat = winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`);
-
 /*
  * Log Level
  * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
  */
 const logger = winston.createLogger({
   format: winston.format.combine(
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.align(),
+    winston.format.printf(info => {
+      const { timestamp, level, message, ...args } = info;
+
+      const ts = timestamp.slice(0, 19).replace('T', ' ');
+      return `${ts} [${level}]: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
     }),
-    logFormat,
   ),
   transports: [
     // debug log setting
